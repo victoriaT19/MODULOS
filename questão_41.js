337885c3e11fc52bdab024c47ebf122316f390a1
victoriaT19 <76068991+victoriaT19@users.noreply.github.com>
2024-06-14 01:21:24 -0300

    Create questão_41.js

---
+++ questão_41.js
@@ -0,0 +1,14 @@
+// Definição inicial do objeto pessoa
+let pessoa = {
+    nome: "João",
+    idade: 30
+};
+
+// Acessando e imprimindo o valor da propriedade idade
+console.log("Idade:", pessoa.idade);
+
+// Adicionando uma nova propriedade email ao objeto pessoa
+pessoa.email = "joao@example.com";
+
+// Imprimindo o objeto pessoa atualizado
+console.log("Objeto pessoa com email:", pessoa);
