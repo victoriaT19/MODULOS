2b2651208be8df80f67b09897a6bdb8b090e98bd
Victória Tauanny de Paula da Silva <76068991+victoriaT19@users.noreply.github.com>
2024-11-14 22:19:00 -0300

    Create q3.java

---
+++ lista7_java/q3.java
@@ -0,0 +1,63 @@
+class editor{
+    private node head;
+    private int tam;
+
+    private static class node{
+        String texto;
+        node next;
+
+        node(String texto){
+            this.texto = texto;
+            this.next = null;
+        }
+    }
+
+    public editor(){
+        this.head = null;
+        this.tam = 0;
+    }
+
+    //funciona como uma pilha
+
+    public void Do(String texto){
+        node newNode = new node(texto);
+        newNode.next = head;
+        head = newNode;
+        tam++;
+        System.out.println("Ação Realizada: " + texto);
+    }
+    public void undo(){
+        if (head == null){
+            System.out.println("Nenhuma ação para desfazer");
+            return;
+        }
+
+        System.out.println("Desfazendo a ação: " + head.texto);
+        head = head.next;
+        tam--;
+    }
+    public void printar(){ //printar as ações realizadas
+        node cur = head;
+        System.out.println("Histórico:");
+        while(cur != null){
+            System.out.println(cur.texto);
+            cur = cur.next;
+        }
+    }
+
+    public static void main(String[] args){
+        editor Editor = new editor();
+
+        Editor.Do("Olá, mundo");
+        Editor.Do("Bem vindos ao edito de texto");
+        Editor.Do("Desfaça agora");
+
+        Editor.printar();
+
+        Editor.undo();
+        Editor.printar();
+
+        Editor.undo();
+        Editor.printar();
+    }
+}
